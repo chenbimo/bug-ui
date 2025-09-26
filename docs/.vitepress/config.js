5d00a8e1e8ce19d0acc0e18505ad9005b889bedf
陈随易 <bimostyle@qq.com>
2025-09-27 02:15:57 +0800

    1

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vitepress';
+
+export default defineConfig({
+    lang: 'zh-CN',
+    title: 'Buig UI',
+    description: 'Vue3 组件库（MVP 重构中）',
+    srcDir: 'markdown',
+    themeConfig: {
+        nav: [],
+        sidebar: []
+    }
+});
